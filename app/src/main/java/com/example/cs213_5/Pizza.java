132d694f3ba81512e14d7eec0e394d1f3fdcb6fe
bzhanggg <blz19@scarletmail.rutgers.edu>
2022-12-04 11:03:28 -0500

    re-add projet 4 files

---
+++ app/src/main/java/com/example/cs213_5/Pizza.java
@@ -0,0 +1,66 @@
+package com.example.cs213_5;
+import javafx.collections.ObservableList;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+/**
+ * The Pizza abstract class represents a Pizza in the pizza creation system. A Pizza
+ * is specified by the type of crust, its size, and the toppings.
+ */
+public abstract class Pizza implements Customizable {
+    /** The list of toppings of a Pizza */
+    protected ObservableList<Topping> toppings;
+    /** The type of crust of a Pizza */
+    protected Crust crust;
+    /** The size of a Pizza */
+    protected Size size;
+    /** The name of the pizza */
+    protected String name;
+
+    /**
+     * The price method calculates the price of a given Pizza.
+     * @return the price of a Pizza.
+     */
+    public abstract double price();
+
+    /**
+     * The setSize method sets the size of a given Pizza.
+     * @param size the size to change the Pizza to.
+     */
+    public void setSize(Size size) {
+        this.size = size;
+    }
+
+    /**
+     * The getToppings method gives the toppings of a pizza.
+     * @return the toppings of a Pizza.
+     */
+    public  ObservableList<Topping> getToppings() {
+        return toppings;
+    }
+
+    /**
+     * The getCrust method gives the crust of a pizza.
+     * @return the crust of a Pizza.
+     */
+    public Crust getCrust() {
+        return crust;
+    }
+
+    /**
+     * Creates a string containing the pizza name, its size, and the toppings of the pizza.
+     * @return String of format "[Pizza size] [NY/Chicago] [Pizza flavor] - [Toppings, ]"
+     */
+    @Override
+    public String toString() {
+        String result = size + " " + name;
+        for(int i = 0; i < toppings.size(); i++) {
+            String begin = i == 0 ? " - " : "";
+            String end = i == toppings.size()-1? "" : ", ";
+            result += begin + toppings.get(i) + end;
+        }
+
+        return result;
+    }
+}
