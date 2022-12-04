ff07687fd1eb0fe6e4c3c5a3bbd527bf0bd76a6d
bzhanggg <blz19@scarletmail.rutgers.edu>
2022-12-04 11:29:22 -0500

    set size and flavor spinners

--- app/src/main/java/com/example/cs213_5/ChicagoFragment.java
+++ app/src/main/java/com/example/cs213_5/ChicagoFragment.java
@@ -3,9 +3,13 @@ package com.example.cs213_5;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
-import android.view.View;
+import android.view.*;
 import android.view.ViewGroup;
 import android.widget.*;
+import android.R.layout;
+
+import java.sql.Array;
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,7 +26,7 @@ public class ChicagoFragment extends Fragment {
     private ImageView pizzaImg;
     private Spinner flavors, size;
     private ListView availableToppings, selectedToppings;
-    private Button addButton, removeButton, orderButton;
+    private Button orderButton;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -66,12 +70,65 @@ public class ChicagoFragment extends Fragment {
         orderButton = view.findViewById(R.id.orderButton);
         availableToppings = view.findViewById(R.id.availableToppings);
         selectedToppings = view.findViewById(R.id.selectedToppings);
-        flavors = view.findViewById(R.id.flavors);
+
         size = view.findViewById(R.id.size);
+        ArrayAdapter<CharSequence> sizeAdapter = ArrayAdapter.createFromResource(getContext(), R.array.size_array, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
+        sizeAdapter.setDropDownViewResource(androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
+        size.setAdapter(sizeAdapter);
+
+        flavors = view.findViewById(R.id.flavors);
+        ArrayAdapter<CharSequence> flavorAdapter = ArrayAdapter.createFromResource(getContext(), R.array.flavor_array, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
+        flavorAdapter.setDropDownViewResource(androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
+        size.setAdapter(flavorAdapter);
+
         pizzaImg = view.findViewById(R.id.pizzaImage);
 
-        
+        orderButton.setOnClickListener(onOrderClick);
+        size.setOnItemSelectedListener(onSizeSelect);
         // Inflate the layout for this fragment
         return view;
     }
+
+    private View.OnClickListener onOrderClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(getActivity(), "Pizza added to order", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    private AdapterView.OnItemSelectedListener onSizeSelect = new AdapterView.OnItemSelectedListener() {
+        @Override
+        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+            switch (i) {
+                case 0:
+                    Toast.makeText(getActivity(), "Small size selected", Toast.LENGTH_SHORT).show();
+                case 1:
+                    Toast.makeText(getActivity(), "Medium size selected", Toast.LENGTH_SHORT).show();
+                case 2:
+                    Toast.makeText(getActivity(), "Large size selected", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> adapterView) {}
+    };
+
+    private AdapterView.OnItemSelectedListener onFlavorSelect = new AdapterView.OnItemSelectedListener() {
+        @Override
+        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+            switch (i) {
+                case 0:
+                    Toast.makeText(getActivity(), "Build Your Own selected", Toast.LENGTH_SHORT).show();
+                case 1:
+                    Toast.makeText(getActivity(), "Deluxe selected", Toast.LENGTH_SHORT).show();
+                case 2:
+                    Toast.makeText(getActivity(), "BBQ Chicken selected", Toast.LENGTH_SHORT).show();
+                case 3:
+                    Toast.makeText(getActivity(), "Meatzza selected", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> adapterView) {}
+    };
 }
