d635d8b3c9a61b813b34bbf0bc5981d4ae5f03c3
mute <janmarzan11@gmail.com>
2022-12-06 15:16:36 -0500

    pizzas are now displayed in an order's order card

--- app/src/main/java/com/example/cs213_5/StoreRecyclerViewAdapter.java
+++ app/src/main/java/com/example/cs213_5/StoreRecyclerViewAdapter.java
@@ -1,6 +1,10 @@
 package com.example.cs213_5;
 
 import android.content.Context;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.StyleSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -34,9 +38,44 @@ public class StoreRecyclerViewAdapter extends RecyclerView.Adapter<StoreRecycler
 
     @Override
     public void onBindViewHolder(@NonNull StoreRecyclerViewAdapter.MyViewHolder holder, int position) {
-        holder.tvName.setText("#" + orders.get(position).getId());
-        holder.tvContents.setText(orders.get(position).getPizzas().size() + " pizzas");
+        holder.tvName.setText("Order #" + orders.get(position).getId());
+        holder.tvContents.setText(generatePizzaString(orders.get(position)), TextView.BufferType.SPANNABLE);
         holder.tvPrice.setText(String.format("$%.2f", orders.get(position).getTotal()));
+    }
+
+    private SpannableStringBuilder generatePizzaString(Order order) {
+        SpannableStringBuilder ssb = new SpannableStringBuilder();
+
+        for(int i = 0; i < order.getPizzas().size(); i++) {
+            Pizza p = order.getPizzas().get(i);
+            StyleSpan boldSpan = new StyleSpan(android.graphics.Typeface.BOLD);
+            ssb.append(p.size.toString() +  " " + p.name + "\n", boldSpan, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            String toAppend;
+            if(i == order.getPizzas().size()-1) {
+                toAppend = generateToppingString(p);
+            } else {
+                toAppend = generateToppingString(p) + "\n";
+            }
+            ssb.append(toAppend);
+        }
+
+        return ssb;
+    }
+
+    private String generateToppingString(Pizza p) {
+        if(p.getToppings().isEmpty()) {
+            return "";
+        }
+
+        String toppingsString = "";
+        ArrayList<Topping> toppings = p.getToppings();
+        for(int i = 0; i < toppings.size(); i++) {
+            String end = i == toppings.size()-1? "" : ", ";
+            end += (i+1)%3 == 0 && i != toppings.size()-1 ? "\n" : "";
+            toppingsString += toppings.get(i) + end;
+        }
+
+        return toppingsString + "\n";
     }
 
     @Override
